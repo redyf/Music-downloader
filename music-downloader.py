635fcf34090c08f873ad1b06ac01bce70df20822
redyf <mateusalvespereira7@gmail.com>
2023-11-23 20:22:15 -0300

    Initial commit

---
+++ music-downloader.py
@@ -0,0 +1,9 @@
+from pytube import YouTube
+
+VIDEO_URL = "https://youtube.com/watch?v=QyiNcEPPb5o&feature=shared"
+yt = YouTube(VIDEO_URL)
+
+
+audio = yt.streams.filter(only_audio=True)[0]
+audio.download()
+
