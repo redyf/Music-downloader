1f75a5512f64cf0259fe7274dcf7c0fd94fd4782
Redyf <mateusalvespereira7@gmail.com>
2024-01-10 13:49:52 -0300

    refactor: Improved version

---
+++ main.py
@@ -0,0 +1,15 @@
+from pytube import YouTube
+
+
+def downloadAudio(VIDEO_URL):
+    convert_url_to_string = str(VIDEO_URL)
+
+
+    yt = YouTube(convert_url_to_string)
+
+    audio = yt.streams.filter(only_audio=True)[0]
+    audio.download()
+
+
+downloadAudio()
+
--- music-downloader.py
+++
@@ -1,9 +0,0 @@
-from pytube import YouTube
-
-VIDEO_URL = "https://youtube.com/watch?v=QyiNcEPPb5o&feature=shared"
-yt = YouTube(VIDEO_URL)
-
-
-audio = yt.streams.filter(only_audio=True)[0]
-audio.download()
-
